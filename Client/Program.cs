5e3ea1784dc599a41ea92e5ec4638a98f461953a
Chris Park <cpark@basistech.com>
2017-02-14 05:13:52 -0800

    Starting point
    - F# project
    - C# client

---
+++ Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rosette;
+
+namespace Rosette {
+    class Program {
+        static void Main(string[] args) {
+            
+            var p = RosetteParameters.DocumentParameters("George Bush")
+                .SetLanguage("eng");            
+
+            var api = Rosette.callEndpoint(Rosette.Endpoint.Ping, "7c3e0f9f51334a0793dde4be37cb22ce", p);
+            
+        }
+
+        
+    }
+}
---
+++ RosetteApi/Endpoints.fs
@@ -0,0 +1,71 @@
+﻿namespace Rosette
+
+open FSharp.Data
+open Microsoft.FSharp.Reflection
+open RosetteParameters
+
+type RosetteResponse = { statusCode: int; headers: System.Collections.Generic.IDictionary<string, string>; body: string }
+
+module Rosette = 
+
+    let GetUnionCaseName (x: 'a) = 
+        match FSharpValue.GetUnionFields(x, typeof<'a>) with
+        | case, _ -> case.Name
+
+    type Endpoint = 
+        | Ping
+        | Info
+        | Entities
+        member this.AsString = 
+            match this with
+            | Ping -> "ping"
+            | Info -> "info"
+            | Entities -> "entities"
+    
+    let (|EndsWithSlash|) (s : string) = s.EndsWith("/")
+
+    let private setUrl url (endpoint: Endpoint) = 
+        match url with
+        | null -> "https://api.rosette.com/rest/v1/" + endpoint.AsString
+        | EndsWithSlash true -> url + endpoint.AsString
+        | _ -> url + "/" + endpoint.AsString
+
+
+    let private rosetteHeaders = [ "Accept", "applicationJson";
+                           "Content-Type", "application/json";
+                           "X-RosetteAPI-Binding", ".NET";
+                           "X-RosetteAPI-Binding-Version", "1.6.0"
+                         ]
+
+    let private getHeaders key = 
+        Seq.append rosetteHeaders  [ "X-RosetteApi-Key", key ]
+
+    let private headers responseHeaders = 
+        responseHeaders |> Map.toSeq |> dict
+
+    let private body responseBody = 
+        match responseBody with
+        | Text text -> text
+        | Binary bytes -> ""
+
+    type Api = {
+        endpoint: Endpoint;
+        apiKey: string;
+        url: string;
+        parameters: Parameters
+        } with 
+        member this.AltUrl url = 
+            {this with url = url}
+        member this.Execute = 
+            let response = Http.Request(url = setUrl this.url this.endpoint, headers = getHeaders this.apiKey, silentHttpErrors = true)
+            {statusCode = response.StatusCode; headers = headers(response.Headers); body = body(response.Body)}
+        
+
+    let callEndpoint endpoint apiKey parameters = 
+        { endpoint = endpoint; apiKey = apiKey; url = "https://api.rosette.com/rest/v1"; parameters = parameters }
+        
+    let entities apikey url = 
+        let response = Http.Request(url = setUrl url Endpoint.Entities, headers = getHeaders apikey, silentHttpErrors = true, httpMethod = "POST")
+
+        {statusCode = response.StatusCode; headers = headers(response.Headers); body = body(response.Body)}
+
